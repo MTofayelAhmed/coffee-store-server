27a3382e2e46f14f06b2150279746aee8b17540e
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-12 02:51:23 +0600

    done

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ app.get('/coffee', async (req, res)=>{
   res.send(result)
 })
 
+app.get('/coffee/:id', async (req, res)=>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await coffeeCollection.findOne(query)
+  res.send(result)
+})
+
 
 
 app.post('/coffee', async(req, res)=>{
